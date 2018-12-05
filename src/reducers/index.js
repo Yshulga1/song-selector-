56c3f065f2bc09dde59e9b13daf6669cae124c7e
Yana Shulga <y@shul.ga>
2018-12-05 14:06:59 +0000

    react-redux folder structure and basics

---
+++ src/reducers/index.js
@@ -0,0 +1,22 @@
+import { combindReducers, combineReducers } from "redux";
+
+const songsReducer = () => {
+  return [
+    { title: "No Scrubs", duration: "4:05" },
+    { title: "Macarena", duration: "2:05" },
+    { title: "All Star", duration: "3:15" },
+    { title: "I Want it that way", duration: "1:05" }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
+
+combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
